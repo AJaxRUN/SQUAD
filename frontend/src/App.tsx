965220bfeb5bcf0b7ed481e8d5e0daa42c9f0d7e
unknown <rn3chy@gmail.com>
2020-06-10 20:26:57 +0530

    pudhu layout da p**da

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {Layout} from './components/layout/layout'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,  
+  Link,
+  Redirect
+} from "react-router-dom";
+import {Login} from './components/loginSignup/login'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+        <Route path="/login" render={() => <Login isAuthed={true} />}/>
+        <Route path="/home" render={()=> <Layout/>} />        
+        <Redirect from="/" to="/login" />
+        </Switch>
+      </Router>
     </div>
   );
 }
